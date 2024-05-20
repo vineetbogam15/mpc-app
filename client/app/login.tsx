e70d723d6a61dcd15ec7101e27550c43296a2238
Vineet Bogam <114532660+V15247@users.noreply.github.com>
2024-05-20 19:11:04 -0400

    User login form added

--- client/app/login.tsx
+++ client/app/login.tsx
@@ -1,19 +1,43 @@
 // app/login.tsx
 import React from "react";
+import { FormEvent } from "react";
+import { useState } from "react";
 import { View, Text } from "react-native";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("John");
+  const [password, setPassword] = useState("Doe");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // Prevent the default form submission behavior
+    // You can perform any form submission logic here
+    console.log("Form submitted");
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#abcdef",
-      }}
-    >
-      <Text>Login Screen</Text>
-    </View>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email:
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label htmlFor="password">
+        Password:
+        <input
+          type="text"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 
